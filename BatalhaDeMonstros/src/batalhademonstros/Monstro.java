b679d4a5f9620629962c9e8e9213a7905d71ebcd
JoaoAV <82339713+JoaoAV@users.noreply.github.com>
2021-05-11 14:01:49 -0300

    - Classe Golpes e Tipo :sparkles: :construction: :rocket:
    - Criei construtores setters e getters para cada elemento e interliguei com a classe tipo e criei funções de fraqueza e vantagem

--- BatalhaDeMonstros/src/batalhademonstros/Monstro.java
+++ BatalhaDeMonstros/src/batalhademonstros/Monstro.java
@@ -1,7 +1,82 @@
 package batalhademonstros;
-
+//Leo
 public class Monstro {
+    private String nome;
+    private Tipo tipo;
+    private int  vida, forca, defesa, velocidade;
+    private Golpe [] golpes;
 
-// cada classe ou funcao foi dividido no whatsapp modifique so o seu por enquanto(apaga esse comentario dps)
+    public Monstro(String nome, Tipo tipo, int vida, int forca, int defesa, int velocidade, Golpe[] golpes) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.vida = vida;
+        this.forca = forca;
+        this.defesa = defesa;
+        this.velocidade = velocidade;
+        this.golpes = golpes;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public Golpe[] getGolpes() {
+        return golpes;
+    }
+
+    public void setGolpes(Golpe[] golpes) {
+        this.golpes = golpes;
+    }
+    //checkar futuramente pra ver se o pokemon ta vivo, talvez tenha que mudar
+    public boolean check() {
+        if (vida > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
