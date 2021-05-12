e0d92b78eab65c25338ef75a91f37a536079fff9
leotarone <83708756+leotarone@users.noreply.github.com>
2021-05-11 22:14:34 -0300

    -Classe Monstro atualizada :rocket: :construction: :bug:
    -Nesse commit criei construtores setters e getters para cada elemento e já deixei uma ligação com a classe golpes como falado no grupo. Também criei uma função que verifica a vida dos monstros.

---
+++ src/batalhademonstros/BatalhaDeMonstros.java
@@ -0,0 +1,7 @@
+package batalhademonstros;
+
+
+public class BatalhaDeMonstros {
+// cada classe ou funcao foi dividido no whatsapp modifique so o seu por enquanto(apaga esse comentario dps)
+
+}
---
+++ src/batalhademonstros/Jogador.java
@@ -0,0 +1,8 @@
+package batalhademonstros;
+
+public class Jogador {
+
+// cada classe ou funcao foi dividido no whatsapp modifique so o seu por enquanto(apaga esse comentario dps)
+
+    
+}
